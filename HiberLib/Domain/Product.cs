4ebd3c88925c16a1e1f9b60e5e3b6f665fe62167
Dimitris @ zeus <dimbaltas@yahoo.gr>
2010-10-17 15:26:34 +0300

    Initial Repository

---
+++ HiberLib/Domain/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HiberLib.Domain
+{
+	class Product
+	{
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public bool Discontinued { get; set; }
+	}
+}
