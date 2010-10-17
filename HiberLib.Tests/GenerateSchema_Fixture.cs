3b4104f6ccf4d4b1b465b85dc250bd4c1dc5f85c
Dimitris @ zeus <dimbaltas@yahoo.gr>
2010-10-17 17:09:17 +0300

    HURRAAYYYY creates table using nunit

---
+++ HiberLib.Tests/GenerateSchema_Fixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiberLib.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace HiberLib.Tests
+{
+    [TestFixture]
+    public class GenerateSchema_Fixture
+    {
+        [Test]
+        public void Can_generate_schema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof (Product).Assembly);
+
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+
+    }
+
+}
--- HiberLib/Domain/Product.cs
+++ HiberLib/Domain/Product.cs
@@ -5,8 +5,9 @@ using System.Text;
 
 namespace HiberLib.Domain
 {
-	class Product
+	public class Product
 	{
+		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Category { get; set; }
 		public bool Discontinued { get; set; }
