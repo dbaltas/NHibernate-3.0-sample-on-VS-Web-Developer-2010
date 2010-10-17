06734b7f5127364843b55d8047cfe4c3a460b3b8
Dimitris @ zeus <dimbaltas@yahoo.gr>
2010-10-17 21:16:33 +0300

    external dlls in references folder

--- HiberLib.Tests/ProductRepository_Fixture.cs
+++ HiberLib.Tests/ProductRepository_Fixture.cs
@@ -18,6 +18,27 @@ namespace HiberLib.Tests
 		private ISessionFactory _sessionFactory;
 		private Configuration _configuration;
 
+		private readonly Product[] _products = new[]
+		{
+			new Product {Name = "Melon", Category = "Fruits"},
+			new Product {Name = "Pear", Category = "Fruits"},
+			new Product {Name = "Milk", Category = "Beverages"},
+			new Product {Name = "Coca Cola", Category = "Beverages"},
+			new Product {Name = "Pepsi Cola", Category = "Beverages"},
+		};
+
+		private void CreateInitialData()
+		{
+			using (ISession session = _sessionFactory.OpenSession())
+			using (ITransaction transaction = session.BeginTransaction())
+			{
+				foreach (var product in _products)
+					session.Save(product);
+
+				transaction.Commit();
+			}
+		}
+
 
 		[TestFixtureSetUp]
 		public void TestFixtureSetUp()
@@ -27,11 +48,12 @@ namespace HiberLib.Tests
 			_configuration.AddAssembly(typeof(Product).Assembly);
 			_sessionFactory = _configuration.BuildSessionFactory();
 		}
-		
+
 		[SetUp]
 		public void SetupContext()
 		{
 			new SchemaExport(_configuration).Execute(true, true, false);
+			CreateInitialData();
 		}
 
 		[Test]
@@ -51,6 +73,75 @@ namespace HiberLib.Tests
 				Assert.AreEqual(product.Name, fromDb.Name);
 				Assert.AreEqual(product.Category, fromDb.Category);
 			}
+		}
+
+		[Test]
+		public void Can_update_existing_product()
+		{
+			var product = _products[0];
+			product.Name = "Yellow Pear";
+			IProductRepository repository = new ProductRepository();
+			repository.Update(product);
+			
+			// use session to try to load the product
+			using (ISession session = _sessionFactory.OpenSession())
+			{
+				var fromDb = session.Get<Product>(product.Id);
+				Assert.AreEqual(product.Name, fromDb.Name);
+			}
+		}
+
+		[Test]
+		public void Can_remove_existing_product()
+		{
+			var product = _products[0];
+			IProductRepository repository = new ProductRepository();
+			repository.Remove(product);
+			using (ISession session = _sessionFactory.OpenSession())
+			{
+				var fromDB = session.Get<Product>(product.Id);
+				Assert.IsNull(fromDB);
+			}
+		}
+
+		[Test]
+		public void Can_get_existing_product_by_id()
+		{
+			IProductRepository repository = new ProductRepository();
+			var fromDb = repository.GetById(_products[1].Id);
+			Assert.IsNotNull(fromDb);
+			Assert.AreNotSame(_products[1], fromDb);
+			Assert.AreEqual(_products[1].Name, fromDb.Name);
+		}
+
+		[Test]
+		public void Can_get_existing_product_by_name()
+		{
+			IProductRepository repository = new ProductRepository();
+			var fromDb = repository.GetByName(_products[1].Name);
+
+			Assert.IsNotNull(fromDb);
+			Assert.AreNotSame(_products[1], fromDb);
+			Assert.AreEqual(_products[1].Id, fromDb.Id);
+		}
+
+		[Test]
+		public void Can_get_existing_products_by_category()
+		{
+			IProductRepository repository = new ProductRepository();
+			var fromDb = repository.GetByCategory("Fruits");
+
+			Assert.AreEqual(2, fromDb.Count);
+			Assert.IsTrue(IsInCollection(_products[0], fromDb));
+			Assert.IsTrue(IsInCollection(_products[1], fromDb));
+		}
+
+		private bool IsInCollection(Product product, ICollection<Product> fromDb)
+		{
+			foreach (var item in fromDb)
+				if (product.Id == item.Id)
+					return true;
+			return false;
 		}
 	}
 }
--- HiberLib/Repositories/ProductRepository.cs
+++ HiberLib/Repositories/ProductRepository.cs
@@ -5,45 +5,73 @@ using System.Text;
 using HiberLib.Domain;
 
 using NHibernate;
+using NHibernate.Criterion;
 
 namespace HiberLib.Repositories
 {
-	public class ProductRepository: IProductRepository
-    {
-        public void Add(Product product)
-        {
+	public class ProductRepository : IProductRepository
+	{
+		public void Add(Product product)
+		{
 			using (ISession session = NHibernateHelper.OpenSession())
 			using (ITransaction transaction = session.BeginTransaction())
 			{
 				session.Save(product);
 				transaction.Commit();
 			}
-        }
- 
-        public void Update(Product product)
-        {
-			throw new NotImplementedException();
-        }
- 
-        public void Remove(Product product)
-        {
-            throw new NotImplementedException();
-        }
- 
-        public Product GetById(Guid productId)
-        {
-            throw new NotImplementedException();
-        }
- 
-        public Product GetByName(string name)
-        {
-            throw new NotImplementedException();
-        }
- 
-        public ICollection<Product> GetByCategory(string category)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		}
+
+		public void Update(Product product)
+		{
+			using (ISession session = NHibernateHelper.OpenSession()) {
+				using (ITransaction transaction = session.BeginTransaction()) {
+					session.Update(product);
+					transaction.Commit();
+				}
+			}
+		}
+
+		public void Remove(Product product)
+		{
+			using (ISession session = NHibernateHelper.OpenSession())
+			{
+				using (ITransaction transaction = session.BeginTransaction())
+				{
+					session.Delete(product);
+					transaction.Commit();
+				}
+			}
+		}
+
+		public Product GetById(Guid productId)
+		{
+			using (ISession session = NHibernateHelper.OpenSession())
+				return session.Get<Product>(productId);
+		}
+
+		public Product GetByName(string name)
+		{
+			using (ISession session = NHibernateHelper.OpenSession())
+			{
+				Product product = session
+					.CreateCriteria(typeof(Product))
+					.Add(Restrictions.Eq("Name", name))
+					.UniqueResult<Product>();
+				return product;
+			}
+		}
+
+		public ICollection<Product> GetByCategory(string category)
+		{
+			using (ISession session = NHibernateHelper.OpenSession())
+			{
+				var products = session.
+					CreateCriteria(typeof(Product)).
+					Add(Restrictions.Eq("Category", category)).List<Product>();
+
+				return products;
+			}
+		}
+	}
 }
 
